61abf392bea9476c733ffa2728c3acd7d86c5284
Sharmake Awil <sharmarke.awil@hotmail.com>
2023-11-07 15:51:16 +0100

    react router version 6

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Home from "./components/Homepage/Homepage";
 import Card from "./components/Card/Card";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/App" element={<App />} />
-        <Route path="/Card/:pokemonName" element={<Card />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/App",
+    element: <App />,
+  },
+  {
+    path: "/Card/:pokemonName",
+    element: <Card />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );

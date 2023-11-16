0b763e1fcde769730463800da7763fab174445ce
Sharmake Awil <sharmarke.awil@hotmail.com>
2023-11-16 09:37:32 +0100

    afficher les pokemon sur le home page

--- frontend/src/components/Homepage/Homepage.jsx
+++ frontend/src/components/Homepage/Homepage.jsx
@@ -1,24 +1,28 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import Favoris from "../Favoris/Favoris";
 
 function Home() {
   return (
     <>
       <Navbar />
-      <div id="div-image">
-        <Link to="/App">
-          <img
-            id="homeImage"
-            src="src/assets/homepicture.png"
-            alt="pokemon click me "
-          />
-        </Link>
-        <h2 id="homeParagraph">
-          It’s empty ! <br />
-          Catch’em Now !
-        </h2>
-      </div>
+      {<Favoris /> !== null ? (
+        <Favoris />
+      ) : (
+        <div id="div-image">
+          <Link to="/App">
+            <img
+              id="homeImage"
+              src="src/assets/homepicture.png"
+              alt="pokemon click me "
+            />
+          </Link>
+          <h2 id="homeParagraph">
+            It’s empty ! <br />
+            Catch’em Now !
+          </h2>
+        </div>
+      )}
     </>
   );
 }
